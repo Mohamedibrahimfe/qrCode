33f1cee72cfd8028b40b6acc4f8f91daa8193044
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-04-27 02:17:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+const img = document.querySelector('#img');
+img.src ="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example/?background=transparent";
+// img.classList.toggle('.show');
